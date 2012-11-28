758912e44cfd3df24bb2eb8d9535251179bdaf8d
Chad Etzel <jazzychad@gmail.com>
2012-11-27 17:20:11 -0800

    Initial Commit

---
+++ CHUIKit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CHUIKit
+//
+//  Created by Chad Etzel on 11/27/12.
+//  Copyright (c) 2012 Chad Etzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
